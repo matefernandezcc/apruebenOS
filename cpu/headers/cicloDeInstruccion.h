3c18be3d2dd10b32ae49e0dd01db3eb4b86737c9
lucasmartin2004 <lucmartin@frba.utn.edu.ar>
2025-04-21 14:00:29 +0000

    Refactor en ciclo de instruccion para CPU, func_init_proc.

--- cpu/headers/cicloDeInstruccion.h
+++ cpu/headers/cicloDeInstruccion.h
@@ -2,24 +2,12 @@
 #define CICLO_DE_INSTRUCCION_H
 #include "sockets.h"
 
-
-
-
 void ejecutar_ciclo_instruccion(int pc, int pid);
-t_instruccion* fetch(int pc, int pid);
-op_code decode(char* instruccion);
-void execute(op_code tipo_instruccion, t_instruccion* instruccion);
-void func_noop();
-void func_write(char* direccion, char* datos);
-void func_read(int direccion, int tamanio);
-void func_goto(char* valor);
-void func_io(char* tiemp);
-void func_init_proc(t_instruccion* instruccion);
-void func_dump_memory();
-void func_exit();
-void pedir_funcion_memoria();
-void check_interrupt();
 t_instruccion* recibir_instruccion(int conexion);
-
+extern int seguir_ejecutando;
+extern int pid_ejecutando;
+extern int pid_interrupt;
+extern int hay_interrupcion;
+extern int pc;
 
 #endif
