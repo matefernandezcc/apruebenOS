160cc46bc577e7bb0a24415313ccf47193aa0195
elucangioli <elucangioli@frba.utn.edu.ar>
2025-05-04 21:45:30 +0000

    arreglo headers

--- cpu/headers/cicloDeInstruccion.h
+++ cpu/headers/cicloDeInstruccion.h
@@ -2,9 +2,9 @@
 #define CICLO_DE_INSTRUCCION_H
 #include "sockets.h"
 
-void ejecutar_ciclo_instruccion(int pc, int pid);
+void ejecutar_ciclo_instruccion();
 t_instruccion* recibir_instruccion(int conexion);
-t_instruccion* fetch(int pc, int pid);
+t_instruccion* fetch();
 op_code decode(char* instruccion);
 void execute(op_code tipo_instruccion, t_instruccion* instruccion);
 void check_interrupt();
