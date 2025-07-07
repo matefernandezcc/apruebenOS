88cd7799b20a18440f490f863683d7ea0e25627c
NicolasPazz <paz-nico@hotmail.com>
2025-07-07 23:52:18 +0000

    Agrego archivos de prueba
    Co-authored-by: Lucas Augusto Martin <lucasmartin2004@users.noreply.github.com>

--- utils/headers/colores.h
+++ utils/headers/colores.h
@@ -2,10 +2,10 @@
 
 // Colors en formato RGB con ANSI
 #define ANSI_COLOR_RESET   "\033[0m"
-#define ANSI_COLOR_ROJO    "\033[38;2;192;31;38m"
+#define ANSI_COLOR_ROJO    "\033[38;2;200;100;120m"
 #define ANSI_COLOR_VERDE   "\033[38;2;179;236;111m"
 #define ANSI_COLOR_AZUL    "\033[38;2;108;182;182m"
-#define ANSI_COLOR1    "\033[38;2;239;228;77m"
+#define ANSI_COLOR1        "\033[38;2;200;150;120m"
 
 // Macros para formatear texto
 #define ROJO(texto)    ANSI_COLOR_ROJO texto ANSI_COLOR_RESET
